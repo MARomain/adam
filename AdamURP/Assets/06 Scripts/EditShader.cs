6d7ff73fd357dc1fae0bd8f3e2fa6a3750a8c58f
MARomain <33660355+MARomain@users.noreply.github.com>
2020-02-19 18:49:28 +0100

    Push nouveau projet LWRP + premier shader concluant pour les outlines, léger debug sur Player.CS également

---
+++ AdamURP/Assets/06 Scripts/EditShader.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+
+public class EditShader : MonoBehaviour
+{
+    public Material outlineMaterial;
+
+    public Slider depthSlider;
+    public Slider normalSlider;
+    public Slider colorSlider;
+    public Slider lineThickness;
+
+    public float depthDefaultValue;
+    public float normalDefaultValue;
+    public float colorDefaultValue;
+    public float thicknessDefaultValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        outlineMaterial = GetComponent<MeshRenderer>().material;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        outlineMaterial.SetFloat("Outline Thickness",lineThickness.value);
+        outlineMaterial.SetFloat("Depth Sensitivity",depthSlider.value);
+        outlineMaterial.SetFloat("Normals Sensitivity",normalSlider.value);
+        outlineMaterial.SetFloat("Color Sensitivity",colorSlider.value);
+
+
+        if(Keyboard.current[Key.R].wasReleasedThisFrame)
+        {
+            outlineMaterial.SetFloat("OutlineThickness", lineThickness.value);
+            outlineMaterial.SetFloat("DepthSensitivity", depthSlider.value);
+            outlineMaterial.SetFloat("NormalsSensitivity", normalSlider.value);
+            outlineMaterial.SetFloat("ColorSensitivity", colorSlider.value);
+        }
+    }
+}
