4f95e69e4bfa9bde4f280397d6a874f245eef4be
MARomain <33660355+MARomain@users.noreply.github.com>
2020-03-10 17:33:58 +0100

    Commit de ma tentative de décor que j'ai oublié de commit precedemment ainsi que d'updates de camera

---
+++ AdamURP/Assets/06 Scripts/TriggerTimeline.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class TriggerTimeline : MonoBehaviour
+{
+    public PlayableDirector timeline;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //timeline = GetComponent<PlayableDirector>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            timeline.Play();
+            //disable player inputs
+        }
+    }
+}
